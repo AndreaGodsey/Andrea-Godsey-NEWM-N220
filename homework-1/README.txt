d55122d6cbb177be38af0b63de88f8b8e98de493
Andrea Godsey <noirmenace101@gmail.com>
2024-09-04 23:57:13 -0400

    New Homework

--- homework-1/README.txt
+++ homework-1/README.txt
@@ -1,4 +1,4 @@
 Name: Andrea Godsey
 Date: 9/3/24
-Live URL: https://andreagodsey.github.io/Andrea-Godsey-NEWM-N220/homework-1
+Live URL: https://andreagodsey.github.io/Andrea-Godsey-NEWM-N220/tree/main/homework-1
 Description: This page is for the Homework 1 Assignment. It is called test.html.
---
+++ homework-2/app.js
@@ -0,0 +1,47 @@
+let addToNum = 2;
+      addToNum += 1;
+      document.getElementById("ticketNum").innerHTML += " 2 += 1 = " + addToNum;
+
+document.getElementById("ticketCost").innerHTML += "1 x $14.00 = $" + 1 * 14.00;
+
+document.getElementById("ttCost").innerHTML += "3 x $14.00 = $" + 3 * 14.00;
+
+let addToNumOne = 35;
+      addToNumOne += 75;
+      addToNumOne += 60;
+      document.getElementById("totalCostOutfit").innerHTML += " $35 += $75 += $60 = $" + addToNumOne;
+
+      let subToNumOne = 235.87;
+      subToNumOne -= 170;
+      console.log(subToNumOne)
+    document.getElementById("bank").innerHTML += " $235.87 -= $170 = $" + subToNumOne ;
+
+
+    document.getElementById("totalPizza").innerHTML += "4 x 8 = " + 4 * 8;
+    document.getElementById("totalStudents").innerHTML += "32 / 2.5 = " + 32 / 2.5;
+    document.getElementById("totalSlices").innerHTML += "12 x 2.5 = " + 12 * 2.5;
+    let subToNumTwo = 32;
+      subToNumTwo -= 30;
+    document.getElementById("profPizza").innerHTML += " 32 -= 30 = " + subToNumTwo ;
+     
+
+
+
+
+document.getElementById("montyAdult").innerHTML += "2 x $12.00 = $" + 2 * 12.00;
+document.getElementById("montyChild").innerHTML += "1 x $6.00 = $" + 1 * 6.00;
+document.getElementById("montyDrinks").innerHTML += "3 x $1.50 = $" + 3 * 1.50;
+
+let addToNumTwo = 24;
+      addToNumTwo += 6;
+      addToNumTwo += 4.5;
+      document.getElementById("montyTotal").innerHTML += " $24 += $6 += $4.50 = $" + addToNumTwo;
+
+let addToNumThree = 202.45;
+      addToNumThree += 134.97;
+      addToNumThree += 256.63;
+      addToNumThree += 178.22;
+      document.getElementById("tipsTotal").innerHTML += " $202.45 += $134.97 += $256.63 += 178.22 = $" + addToNumThree;
+
+document.getElementById("tipsAverage").innerHTML += "$772.27 / 4 = $" + 772.27 / 4;
+
