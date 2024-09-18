fdca29c7128896791306c3b9911a078611518e9f
Andrea Godsey <noirmenace101@gmail.com>
2024-09-18 18:15:04 -0400

    Fixing Interest Calculator

--- homework-3/app.js
+++ homework-3/app.js
@@ -2,7 +2,9 @@ function calctotalInterest() {
     const principle = document.getElementById("principle").value;
     const rate = document.getElementById("rate").value;
     const time = document.getElementById("time").value;
+    const answer = principle * (1 + rate * time);
+    const resultsAfterAnswer = answer - principle;
     
-    document.querySelector("#results").innerHTML += "Interest: " + principle * 1 + rate * time + "<br/>";
+    document.querySelector("#results").innerHTML += `With a beginning principal of $${principle} and with a growth rate of ${rate}% for ${time} years, your total interest will be$ ${resultsAfterAnswer.toFixed(2)} with a grand total of $${answer.toFixed(2)}.`
 }
 
