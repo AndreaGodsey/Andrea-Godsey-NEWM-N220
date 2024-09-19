16a21e0b4626956a67d141f86a5568576dcecbbf
Andrea Godsey <noirmenace101@gmail.com>
2024-09-18 22:35:36 -0400

    Adding Link

---
+++ homework-3/README.txt
@@ -0,0 +1,4 @@
+Name: Andrea Godsey
+Date: 9/18/24
+Live URL: https://andreagodsey.github.io/Andrea-Godsey-NEWM-N220/homework-3/interestCalculator.html
+Description: This website is an interest calculator. Users can calculate interest by pressing the calculate button and receiving a result sentence.
--- homework-3/app.js
+++ homework-3/app.js
@@ -5,6 +5,6 @@ function calctotalInterest() {
     const answer = principle * (1 + rate * time);
     const resultsAfterAnswer = answer - principle;
     
-    document.querySelector("#results").innerHTML += `With a beginning principal of $${principle} and with a growth rate of ${rate}% for ${time} years, your total interest will be$ ${resultsAfterAnswer.toFixed(2)} with a grand total of $${answer.toFixed(2)}.`
+    document.querySelector("#results").innerHTML += `With a beginning principal of $${principle} and with a growth rate of ${rate}% for ${time} years, your total interest will be $${resultsAfterAnswer.toFixed(2)} with a grand total of $${answer.toFixed(2)}.`
 }
 
