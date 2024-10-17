898a4aa4162512e97f6e0a8cd4ab02221b4ad807
Andrea Godsey <noirmenace101@gmail.com>
2024-10-17 11:30:14 -0400

    Division by 100

--- homework-3/app.js
+++ homework-3/app.js
@@ -2,7 +2,7 @@ function calctotalInterest() {
     const principle = document.getElementById("principle").value;
     const rate = document.getElementById("rate").value;
     const time = document.getElementById("time").value;
-    const answer = principle * (1 + rate * time);
+    const answer = principle * (1 + (rate * time/100));
     const resultsAfterAnswer = answer - principle;
     
     document.querySelector("#results").innerHTML += `With a beginning principal of $${principle} and with a growth rate of ${rate}% for ${time} years, your total interest will be $${resultsAfterAnswer.toFixed(2)} with a grand total of $${answer.toFixed(2)}.`
