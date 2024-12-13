65a95fa321509af4b90cc146a5ad5d939fc8dc07
Andrea Godsey <noirmenace101@gmail.com>
2024-12-13 11:19:27 -0500

    Finalizing Code

--- final-project/dashboardaccess.js
+++ final-project/dashboardaccess.js
@@ -1,24 +1,65 @@
+let username = ""
+let usernameHeaderReference =document.querySelector("#usernameHeader")
+let dashboardReference = document.querySelector(".dashboard")
+dashboardReference.style.display = "none"
+let loginReference = document.querySelector(".loginReference")
+const myTasks = [];
+
+//Make your Task and Buttons Appear
+function showTasks() {
+   document.getElementById("tasksList").innerHTML = "";
+   for (let index = 0; index < myTasks.length; index++) {
+    const newTask = myTasks[index];
+    
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span onclick="blackLine(${index})">${newTask}</span> 
+    
+    <button style="background-color: #811d1d;" onclick="removeTask(${index})">Remove</button>
+    
+    <button style="background-color: #8f91d6;"onclick="updateTask(${index})">Change Text</button>
+
+    <label id="chooseCategory">No Category</label>
+        <select id="categoryList" onchange="updateCategory(${index})">
+            <option value="No Category">No Category</option>
+            <option value="Indoor">Indoor</option>
+            <option value="Outdoor">Outdoor</option>
+        </select>
+
+
+    
+    `; 
+     document.getElementById("tasksList").appendChild(li);
+   }
+
+}
+
+
+//Make sure username and password meets the quota
 function validateLogin() {
-    document.getElementById('usernameError').textContent = '';
-    document.getElementById('passwordError').textContent = '';
+
+    document.getElementById('usernameError').innerHTML = '';
+    document.getElementById('passwordError').innerHTML = '';
     
     let containsNumber = false;
     let containsUppercase = false;
 
 
+
     const usernameReference = document.getElementById("username").value;
     const passwordReference = document.getElementById("password").value;
 
+    //Username cannot be empty
     if (usernameReference === '') {
         document.getElementById('usernameError').innerHTML = 'Username box cannot be empty!';
     }
-
+    //Password cannot be empty
     if (passwordReference === '') {
         document.getElementById('passwordError').innerHTML = 'Password box cannot be empty!';
     }
 
     console.log(passwordReference.length)
-
+    //Password must be 8 characters
     if (passwordReference > 8) {
         document.getElementById('passwordError').innerHTML ='Password must be at least 8 characters!'
     }
@@ -29,28 +70,102 @@ function validateLogin() {
         // Is there at least 1 number
         if (isNaN(currentCharacter) === false) {
             containsNumber = true;
-          } else {
-            document.getElementById('passwordError').innerHTML ='Password must have at least 1 number!'
-          }
+          } 
         // Is there at least 1 capital letter
         if (
           currentCharacter == currentCharacter.toUpperCase() &&
           isNaN(currentCharacter)
         ) {
           containsUppercase = true;
-        } else{
-            document.getElementById('passwordError').innerHTML ='Password must have at least 1 capital letter!'
-        }
+        } 
 
       }
 
       const validPassword = containsNumber && containsUppercase;
 
     if (validPassword) {
-        window.location.href = 'taskaccount.html';
+       dashboardReference.style.display = "block";
+       loginReference.style.display = "none";
+       username = usernameReference;
+       usernameHeaderReference.innerHTML = username
+            
+          ;
       passwordReference = "";
     } else {
       alert("Login Unsuccessful");
+
+    //   add logic to show error message
+    if (containsNumber === false) {
+         document.getElementById('passwordError').innerHTML ='Password must have at least 1 number!'
+    }
+    if (containsUppercase === false ) {
+document.getElementById('passwordError').innerHTML ='Password must have at least 1 capital letter!'
+    }
     }
     
     }
+
+
+    //Logout of Application
+function returnHome() {
+    dashboardReference.style.display = "none";
+       loginReference.style.display = "block";
+}
+
+//Adding Tasks
+document.getElementById("addingTask").addEventListener("click", function() {
+   const newTask = prompt(`New Task`);
+   if (newTask) {
+   
+    myTasks.push(newTask);
+    showTasks(); 
+}
+        });
+
+ //Removing Tasks
+function removeTask(taskIndex) {
+    myTasks.splice(taskIndex, 1);
+    showTasks();
+          };
+
+//Update Tasks
+function updateTask(taskIndex) {
+    const changeText = prompt("Change Your Task?", myTasks[taskIndex]);
+    if (changeText !== null && changeText !== "") {
+        myTasks[taskIndex] = changeText;
+    }
+    showTasks();
+    };
+
+//Change Properties
+function updateCategory(taskIndex) {
+const selectedValue = document.getElementById('categoryList').value;
+        
+const taskLabel = document.getElementById('chooseCategory');
+taskLabel.innerHTML = selectedValue;
+    }
+
+//Mark off Tasks
+function blackLine(taskIndex) {
+    const taskText = document.querySelectorAll('#tasksList li span')[taskIndex];
+    
+        
+    if (taskText.style.textDecoration === 'line-through') {
+        taskText.style.textDecoration = 'none'; 
+    } else {
+        taskText.style.textDecoration = 'line-through'; 
+        }
+    }
+
+
+
+        
+
+
+          
+        
+
+        
+
+
+
--- week-8/friends.js
+++ week-8/friends.js
@@ -1,106 +1,85 @@
-//Reference Variables
-const friendNameInput = document.getElementById("friendName");
+// Reference variables
+const friendNameInputRef = document.getElementById("friendName");
 const friendsListRef = document.getElementById("friendsList");
 
 // Variable to track my friends
 const myFriends = [];
 
-//Function for  Adding Friend
+// Function for adding friend
 function addFriend() {
-  //Get the Name the User Typed In
-  const nameValue = friendNameInput.value;
+  // get the name the user typed in
+  const nameValue = friendNameInputRef.value;
 
-  //Add Friend to my Friend's List
-  myFriends.push({name: nameValue, color: "black" });
+  // add friend to myFriends list
+  myFriends.push({ name: nameValue, color: "black" });
 
-  //Add the name to the friends list
   showFriends();
 
-  //Clear the Name the User Typed
-  friendNameInput.value = "";
+  // clear the name the user typed
+  friendNameInputRef.value = "";
 }
 
-function removeFriend(friendName) {
-  //Use variable to track index of friend to remove
-myFriends.splice(friendIndex, 1);
-  let friendIndex;
-
-  //loop through current friends to find the friendName
-  //for (let i = 0; i < myFriends.length; i++) {
-    //if (myFriends[i] === friendName) {
-      //friendIndex = i;
-    //}
-  //}
-
-  console.log(friendIndex);
-
-  //Remove Friends
+function removeFriend(friendIndex) {
+  // remove the person from the list
   myFriends.splice(friendIndex, 1);
-  console.log(myFriends);
+
   showFriends();
 }
 
-//Function to Loop Through My Friends and Show them on Page
+// function to loop through myFriends and show 'em on the page
 function showFriends() {
-  //Clear Any Friends on the Page
+  // clear pages old friends list
   friendsListRef.innerHTML = "";
 
-  //Loop Through Friends
+  // loop through friends
   myFriends.forEach(function (friend, friendIndex) {
+    // add the name to the friends list
     friendsListRef.innerHTML += `<li>
-    <span style="color: ${friend.color}">${friend.name}</span> <button onclick="removeFriend('${friend.name}')">X</button>
-    <button onclick="moveFriend('${friend.name}', true)">&#8593;</button>
-    <button onclick="moveFriend('${friend.name}', false)">&#8595;</button>
-
-    <input type="color" 
-    name="friendTextColor${friendIndex}" 
-    id="friendTextColor${friendIndex}"
-    onchange="changeFriendTextColor('${friendIndex}', event.currentTarget)" />
-
-    </li>`;
+    <span style="color: ${friend.color}">${friend.name}</span>
+    <button onclick="removeFriend(${friendIndex})">X</button>
+    <button onclick="moveFriend(${friendIndex}, true)">&#8593;</button>
+    <button onclick="moveFriend(${friendIndex}, false)">&#8595;</button>
+    
+    <input 
+        type="color" 
+        name="friendTextColor${friendIndex}"
+        id="friendTextColor${friendIndex}"
+        onchange="changeFriendTextColor(${friendIndex}, event.currentTarget)"
+    />
+    
+ </li>`;
   });
 }
 
+function changeFriendTextColor(friendIndex, inputRef) {
+  const friendName = myFriends[friendIndex].name;
+  myFriends.splice(friendIndex, 1, { name: friendName, color: inputRef.value });
+
+  showFriends();
+}
+
+// function to clear list of friends
 function clearList() {
   myFriends.splice(0, myFriends.length);
 
   showFriends();
 }
 
-//functions to move friends ranking
-function moveFriend(friendName, isMovingUp) {
-    let friendIndex;
-    let friendNewIndex;
+// function to move friends ranking
+function moveFriend(friendIndex, isMovingUp) {
+  let friendNewIndex;
 
-    for(let i = 0; i < myFriends.length; i++) {
-        if(myFriends[i].name === friendName) {
-            friendIndex = i;
-            // ['A','B','C'] move B up 1 space
-            // index of  'B' is originally 1
-            //This is what the result should be
-            // ['B', 'A', 'C'] so the new index is 0
-            if(isMovingUp) {
-                friendNewIndex = friendIndex - 1;
-            } else {
-                friendNewIndex = friendIndex + 1;
-               }
-            // ['A','B','C'] move B down 1 space
-            // index of  'B' is originally 1
-             // ['A','C','B'] so new index is 2
-           
-        }
-    }
-//Remove Friend from Old Space
-    myFriends.splice(friendIndex, 1);
-//Add Friend to New Space
-    myFriends.splice(friendNewIndex, 0, { name: friendName, color: "black" });
+  if (isMovingUp) {
+    friendNewIndex = friendIndex - 1;
+  } else {
+    friendNewIndex = friendIndex + 1;
+  }
 
-    showFriends();
-}
+  const friendName = myFriends[friendIndex].name;
+  // remove friend from old space
+  myFriends.splice(friendIndex, 1);
+  // add friend to new space
+  myFriends.splice(friendNewIndex, 0, { name: friendName, color: "black" });
 
-function changeFriendTextColor(friendIndex, inputRef) {
-const friendName = myFriends[friendIndex].name;
-myFriends.splice(friendIndex, 1, { name: friendName, color:inputRef.value })
-
-showFriends();
+  showFriends();
 }
